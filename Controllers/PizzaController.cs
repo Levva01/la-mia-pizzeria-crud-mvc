36d6f838934b1b4353d9ffe35a971a55173ed8b0
Levva01 <vellanifederico13@gmail.com>
2022-10-06 14:52:57 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace la_mia_pizzeria_post.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = new();
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                pizze = db.Pizze.ToList();
+            }
+            return View(pizze);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formPizza)
+        {
+            /*
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                Pizza pizzaCreate = new Pizza();
+                pizzaCreate.Nome = formPizza.Nome;
+                pizzaCreate.Descrizione = formPizza.Descrizione;
+                pizzaCreate.Foto = formPizza.Foto;
+                pizzaCreate.Prezzo = formPizza.Prezzo;
+                context.Pizze.Add(pizzaCreate);
+                context.SaveChanges();
+                return RedirectToAction("Pizza");
+            } */
+
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formPizza);
+            }
+
+            using (var db = new ApplicationDbContext())
+            {
+                db.Pizze.Add(formPizza);
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+
+
+
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,64 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Pizza> Pizze { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db_pizzeria;Integrated Security=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pizza>()
+            .HasData(
+                new Pizza
+                {
+                    Id = 1,
+                    Nome = "Margherita",
+                    Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+                    Foto = "img/pizza.jpg",
+                    Prezzo = 5.00
+                },
+
+                new Pizza
+                {
+                    Id = 2,
+                    Nome = "Margherita",
+                    Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+                    Foto = "img/pizza.jpg",
+                    Prezzo = 5.00
+                },
+
+                new Pizza
+                {
+                    Id = 3,
+                    Nome = "Margherita",
+                    Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+                    Foto = "img/pizza.jpg",
+                    Prezzo = 5.00
+                },
+
+                new Pizza
+                {
+                    Id = 4,
+                    Nome = "Margherita",
+                    Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+                    Foto = "img/pizza.jpg",
+                    Prezzo = 5.00
+                },
+
+                new Pizza
+                {
+                    Id = 5,
+                    Nome = "Margherita",
+                    Descrizione = "Pom. San Marzano D.O.P, Fior di Latte , Olio Evo",
+                    Foto = "img/pizza.jpg",
+                    Prezzo = 5.00
+                });
+    }
+
+}
